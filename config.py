cdcac02c381797e7cd0c4642c713ac3428c56f22
Jeshan04 <63904611+Jeshan04@users.noreply.github.com>
2024-05-08 09:07:53 +0530

    Initial Commit

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+from os import environ
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SECRET_KEY = "JOSHI"
+
+    UPLOADS = "app/static/uploads"
+    SESSION_COOKIE_SECURE = True
+    DEFAULT_THEME = None
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SESSION_COOKIE_SECURE = False
+
+class DebugConfig(Config):
+    DEBUG = False
